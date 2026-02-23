67634a3c9c6a59347059bdd6170ab4fc3d92271b
devesh-narang <77012458+devesh-narang@users.noreply.github.com>
2026-02-23 09:11:13 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp - UC2 Implementation
+ * Rendering "OOPS" as a 7-line ASCII banner.
+ * @author [Your Name]
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Line-by-line rendering of the OOPS banner
+        System.out.println("  ***   ***  ****    ****   ");
+        System.out.println(" *   * *   * *   *  *       ");
+        System.out.println(" *   * *   * *   *  *       ");
+        System.out.println(" *   * *   * ****    ***     ");
+        System.out.println(" *   * *   * *          *    ");
+        System.out.println(" *   * *   * *          *    ");
+        System.out.println("  ***   ***  *       ****   ");
+    }
+}
