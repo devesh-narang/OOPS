9ab1bb8c59df600580775bded0d2539489ec8646
devesh-narang <77012458+devesh-narang@users.noreply.github.com>
2026-02-23 09:04:10 +0530

    Add files via upload

---
+++ UC1.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp - Foundational UC1 Implementation
+ * * @author [Your Name]
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    /**
+     * Entry point of the application.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // UC1: Printing the literal text "OOPS"
+        System.out.println("OOPS");
+    }
+}
